1ceb721771cc6ef933a811fd8981a41aef674b19
k-nautresurrect <kushignite@gmail.com>
2022-02-10 22:13:59 +0530

    Initiated for code section

---
+++ code/src/io/IO.java
@@ -0,0 +1,9 @@
+package io;
+
+import java.util.Scanner;
+import java.io.PrintStream;
+
+public class IO {
+	public static PrintStream out = new PrintStream(System.out);
+	public static Scanner in =  new Scanner(System.in);
+}
---
+++ code/src/tkufrwrdlst/Main.java
@@ -0,0 +1,9 @@
+package tkufrwrdlst;
+
+import io.IO;
+
+public class Main extends IO {
+	public static void main(String[] args) {
+		out.println("Hello world");
+	}
+}
